3f1d28615077a8eda472624cb8ef35adf889dcf5
dev-jorge10 <boostsole@gmail.com>
2024-09-09 16:23:00 -0500

    Initial Commit

--- index.js
+++ index.js
@@ -1,37 +1,66 @@
-let homeStoreEl = document.getElementById("home-score")
-let guestStoreEl = document.getElementById("guest-score")
+const homeStoreEl = document.getElementById("home-score")
+const guestStoreEl = document.getElementById("guest-score")
 
 
-let homeScoreByOne = document.getElementById("home-score-btn-1")
-let homeScoreByTwo = document.getElementById("home-score-btn-2")
-let homeScoreByThree = document.getElementById("home-score-btn-3")
+const homeScoreByOne = document.getElementById("home-score-btn-1")
+const homeScoreByTwo = document.getElementById("home-score-btn-2")
+const homeScoreByThree = document.getElementById("home-score-btn-3")
 
 
-let guestScoreByOne = document.getElementById("guest-score-btn-1")
-let guestScoreByTwo = document.getElementById("guest-score-btn-2")
-let guestScoreByThree = document.getElementById("guest-score-btn-3")
+const guestScoreByOne = document.getElementById("guest-score-btn-1")
+const guestScoreByTwo = document.getElementById("guest-score-btn-2")
+const guestScoreByThree = document.getElementById("guest-score-btn-3")
 
-let newBtn = document.getElementById("new-game-btn")
+const homeHeader = document.querySelector(".home-section h3");
+const guestHeader = document.querySelector(".guest-section h3");
+
+
+const newBtn = document.getElementById("new-game-btn")
+
+
 
 let homeScore = 0
 let guestScore = 0
 
 
 
+function updateScores() {
+    homeStoreEl.textContent = homeScore;
+    guestStoreEl.textContent = guestScore;
+    checkScores();
+}
+
+function checkScores() {
+    if (homeScore > guestScore) {
+        homeHeader.style.color = "#4E9F3D";
+        guestHeader.style.color = "#eeeeee"; // Default color
+    } else if (guestScore > homeScore) {
+        homeHeader.style.color = "#eeeeee"; // Default color
+        guestHeader.style.color = "#4E9F3D";
+    } else {
+        homeHeader.style.color = "#eeeeee"; // Default color
+        guestHeader.style.color = "#eeeeee"; // Default color
+    }
+}
+
+
 
 homeScoreByOne.addEventListener("click", function(){
     homeScore += 1
     homeStoreEl.textContent = homeScore
+    updateScores();
 })
 
  homeScoreByTwo.addEventListener("click", function(){
      homeScore += 2
      homeStoreEl.textContent = homeScore
+     updateScores();
 })
 
  homeScoreByThree.addEventListener("click", function(){
      homeScore += 3
      homeStoreEl.textContent = homeScore
+     updateScores();
 })
 
 
@@ -39,16 +68,19 @@ homeScoreByOne.addEventListener("click", function(){
 guestScoreByOne.addEventListener("click", function(){
       guestScore += 1
       guestStoreEl.textContent = guestScore
+      updateScores();
 })
 
   guestScoreByTwo.addEventListener("click", function(){
      guestScore += 2
      guestStoreEl.textContent = guestScore
+     updateScores();
 })
 
  guestScoreByThree.addEventListener("click", function(){
      guestScore += 3
      guestStoreEl.textContent = guestScore
+     updateScores();
  })
 
  newBtn.addEventListener("click", function(){
@@ -56,4 +88,6 @@ guestScoreByOne.addEventListener("click", function(){
     guestScore = 0
     homeStoreEl.textContent = homeScore
     guestStoreEl.textContent = homeScore
+    updateScores();
  })
+
